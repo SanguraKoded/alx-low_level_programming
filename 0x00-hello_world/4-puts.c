556fb756b300aee7872b92c5dae0bf9e124913c1
SanguraKoded <sanguraj12@gmail.com>
2023-06-15 07:00:35 -0700

    puts c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 int main (void)
 {
 	/**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- */
+	 * main - prints the phrase "Programming is like building a multilingual puzzle
+	 * Return: 0 if exited properly, non-zero otherwise
+	 */
 	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
